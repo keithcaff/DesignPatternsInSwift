a932d67c4cb1b0776256531954683dc3ea7fd995
Keith Caffrey <keithcaffrey@gmail.com>
2017-07-12 18:52:53 +0100

    starting observer pattern work

---
+++ Behavioral/ObserverPattern/ObserverPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObserverPattern
+//
+//  Created by Keith Caffrey on 12/07/2017.
+//  Copyright © 2017 KCDEV. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
