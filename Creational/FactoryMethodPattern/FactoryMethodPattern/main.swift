000fc08ada68b841f0c0c011901c0ad3c5564ce9
Keith Caffrey <keithcaffrey@gmail.com>
2017-07-18 18:47:27 +0100

    skeleton for factory method pattern

---
+++ Creational/FactoryMethodPattern/FactoryMethodPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FactoryMethodPattern
+//
+//  Created by Keith Caffrey on 18/07/2017.
+//  Copyright © 2017 KCDEV. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
