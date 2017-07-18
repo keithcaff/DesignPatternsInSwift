144abe29ecbda9cf6c1adbbcb0439140d22c02d0
Keith Caffrey <keithcaffrey@gmail.com>
2017-07-18 18:07:38 +0100

    starting work on abstract factory pattern

---
+++ Creational/FactoryPattern/FactoryPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FactoryPattern
+//
+//  Created by Keith Caffrey on 18/07/2017.
+//  Copyright © 2017 KCDEV. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
